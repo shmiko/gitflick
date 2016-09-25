4fd02305a59ef51d2573090e99605654f4229b41
shmiko <shmiko@gmail.com>
2016-09-25 17:58:51 +1000

    rc6

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { ProfileComponent } from './components/profile.component'
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: '<h1>My First Angular App</h1><profile></profile>',
+    directives: [ProfileComponent]
 })
 export class AppComponent { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import {ProfileComponent} from './components/profile.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, ProfileComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
