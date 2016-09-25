779a209e8ed7aab5565242635808d70809bd896e
shmiko <shmiko@gmail.com>
2016-09-26 00:04:23 +1000

    rc6

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import { ProfileComponent } from './components/profile.component'
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1><profile></profile>',
+    template: '<h1>My First Angular App.</h1><profile></profile>',
     directives: [ProfileComponent]
 })
 export class AppComponent { }
