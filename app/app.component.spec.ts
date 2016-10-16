2086debc0ef0db20d00ebe6c79c8c5e289cf432a
shmiko <shmiko@gmail.com>
2016-10-16 21:55:09 +1100

    tidy up

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:no-unused-variable */
-import { AppComponent } from './app.component';
+import { AppComponent }   from './app.component';
 
-import { TestBed } from '@angular/core/testing';
+import { TestBed }        from '@angular/core/testing';
 
 import { By }             from '@angular/platform-browser';
 
