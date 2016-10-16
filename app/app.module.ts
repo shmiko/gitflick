104d34c14914619de92b8667012cda0b7bff2c08
shmiko <shmiko@gmail.com>
2016-10-16 21:54:14 +1100

    tidy up

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,7 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
-import {ProfileComponent} from './components/profile.component';
+import { NgModule }       	from '@angular/core';
+import { BrowserModule }  	from '@angular/platform-browser';
+import { AppComponent }   	from './app.component';
+import { ProfileComponent } from './components/profile.component';
 
 @NgModule({
   imports: [ BrowserModule ],
